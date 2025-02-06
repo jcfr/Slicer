f09698d9a46a2850b80a86a803f7d4bab4079269
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2025-02-06 08:16:08 -0500

    ENH: Generalize CompositeNode API introducing "Nth Layer" functions

--- Libs/MRML/Logic/vtkMRMLSliceLogic.cxx
+++ Libs/MRML/Logic/vtkMRMLSliceLogic.cxx
@@ -1098,16 +1098,8 @@ void vtkMRMLSliceLogic::UpdatePipeline()
     // TODO: change logic to use a volume node superclass rather than
     // a scalar volume node once the superclass is sorted out for vector/tensor Volumes
 
-    const char *id;
-
     // Background
-    id = this->SliceCompositeNode->GetBackgroundVolumeID();
-    vtkMRMLVolumeNode *bgnode = nullptr;
-    if (id)
-    {
-      bgnode = vtkMRMLVolumeNode::SafeDownCast (this->GetMRMLScene()->GetNodeByID(id));
-    }
-
+    vtkMRMLVolumeNode *bgnode = this->SliceCompositeNode->GetNthLayerVolume(vtkMRMLSliceLogic::LayerBackground);
     if (this->GetBackgroundLayer())
     {
       if ( this->GetBackgroundLayer()->GetVolumeNode() != bgnode )
@@ -1118,13 +1110,7 @@ void vtkMRMLSliceLogic::UpdatePipeline()
     }
 
     // Foreground
-    id = this->SliceCompositeNode->GetForegroundVolumeID();
-    vtkMRMLVolumeNode *fgnode = nullptr;
-    if (id)
-    {
-      fgnode = vtkMRMLVolumeNode::SafeDownCast (this->GetMRMLScene()->GetNodeByID(id));
-    }
-
+    vtkMRMLVolumeNode *fgnode = this->SliceCompositeNode->GetNthLayerVolume(vtkMRMLSliceLogic::LayerForeground);
     if (this->GetForegroundLayer())
     {
       if ( this->GetForegroundLayer()->GetVolumeNode() != fgnode )
@@ -1135,13 +1121,7 @@ void vtkMRMLSliceLogic::UpdatePipeline()
     }
 
     // Label
-    id = this->SliceCompositeNode->GetLabelVolumeID();
-    vtkMRMLVolumeNode *lbnode = nullptr;
-    if (id)
-    {
-      lbnode = vtkMRMLVolumeNode::SafeDownCast (this->GetMRMLScene()->GetNodeByID(id));
-    }
-
+    vtkMRMLVolumeNode *lbnode = this->SliceCompositeNode->GetNthLayerVolume(vtkMRMLSliceLogic::LayerLabel);
     if (this->GetLabelLayer())
     {
       if ( this->GetLabelLayer()->GetVolumeNode() != lbnode )
