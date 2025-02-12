bfe19062cddf9cdda8a2e627d1981339d0e5a2fa
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2025-02-12 18:50:42 -0500

    ENH: Generalize CompositeNode "opacity" API introducing "Nth Layer" functions

--- Libs/MRML/Logic/vtkMRMLSliceLogic.cxx
+++ Libs/MRML/Logic/vtkMRMLSliceLogic.cxx
@@ -1134,9 +1134,9 @@ void vtkMRMLSliceLogic::UpdatePipeline()
           this->SliceCompositeNode->GetClipToBackgroundVolume(),
           this->GetNthLayerImageDataConnection(vtkMRMLSliceLogic::LayerBackground),
           this->GetNthLayerImageDataConnection(vtkMRMLSliceLogic::LayerForeground),
-          this->SliceCompositeNode->GetForegroundOpacity(),
+          this->SliceCompositeNode->GetNthLayerOpacity(vtkMRMLSliceLogic::LayerForeground),
           this->GetNthLayerImageDataConnection(vtkMRMLSliceLogic::LayerLabel),
-          this->SliceCompositeNode->GetLabelOpacity()
+          this->SliceCompositeNode->GetNthLayerOpacity(vtkMRMLSliceLogic::LayerLabel)
           );
 
     std::deque<SliceLayerInfo> layersUVW;
@@ -1146,9 +1146,9 @@ void vtkMRMLSliceLogic::UpdatePipeline()
           this->SliceCompositeNode->GetClipToBackgroundVolume(),
           this->GetNthLayerImageDataConnectionUVW(vtkMRMLSliceLogic::LayerBackground),
           this->GetNthLayerImageDataConnectionUVW(vtkMRMLSliceLogic::LayerForeground),
-          this->SliceCompositeNode->GetForegroundOpacity(),
+          this->SliceCompositeNode->GetNthLayerOpacity(vtkMRMLSliceLogic::LayerForeground),
           this->GetNthLayerImageDataConnectionUVW(vtkMRMLSliceLogic::LayerLabel),
-          this->SliceCompositeNode->GetLabelOpacity()
+          this->SliceCompositeNode->GetNthLayerOpacity(vtkMRMLSliceLogic::LayerLabel)
           );
 
     // Update operating to perform for add/subtract pipeline
